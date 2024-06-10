634750555cc836b7ccd8221bd6da6d8a967169f2
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2024-06-10 16:37:37 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,106 +1,82 @@
-const box = document.querySelector('.box');
-const game = document.querySelector('.game');
-game.innerHTML = makeMurckup();
-let subBoxes = document.querySelectorAll('.sub-box');
-const overlay = document.querySelector('.overlay');
-const restartButton = document.querySelector('.restart-button');
-const message = document.querySelector('.message');
-const test = document.querySelector('.test');
+const buttons = document.querySelector('.buttons');
+const display = document.querySelector('.display');
 
-const winCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-let winnersName = '';
-let playerX = [];
-let playerO = [];
+buttons.addEventListener('click', onClick);
 
-const markerX =
-  '<div class="markerX"><div class="stick1"></div><div class="stick2"></div></div>';
-const markerO = '<div class="markerO"></div>';
+let a = null;
+let b = null;
+let operation = '';
+let newNumber = true;
 
-let marker = markerX;
-
-box.addEventListener('click', onclick);
-
-function makeMurckup() {
-  let maurckup = '';
-  for (i = 0; i < 9; i += 1) {
-    maurckup += `<div class="sub-box" data-id=${i}></div>`;
+function onClick(evn) {
+  if (evn.target === evn.currentTarget) {
+    return;
   }
-  return maurckup;
-}
-
-function showModal() {
-  restartButton.addEventListener('click', restart);
-  setTimeout(() => {
-    overlay.classList.remove('hide');
-  }, 500);
-}
-function restart() {
-  overlay.classList.add('hide');
-  restartButton.removeEventListener('click', restart);
-  game.innerHTML = makeMurckup();
-  winnersName = '';
-  playerX = [];
-  playerO = [];
-  subBoxes = document.querySelectorAll('.sub-box');
-}
-
-function colorWin(arr) {
-  arr.forEach(element => {
-    [...subBoxes][element].classList.add('winner');
-  });
-}
-
-function checkWin(player) {
-  return winCombinations.find(item => item.every(el => player.includes(el)));
-}
-
-function onclick(evn) {
-  if (
-    !evn.target.classList.contains('sub-box') ||
-    evn.target.firstChild ||
-    winnersName
-  ) {
+  if (display.textContent === '0') {
+    display.textContent = '';
+  }
+  if (evn.target.textContent === '.' && display.textContent.includes('.')) {
     return;
   }
 
-  evn.target.innerHTML = marker;
-
-  if (marker === markerX) {
-    playerX.push(Number(evn.target.dataset.id));
+  if (evn.target.classList.contains('number')) {
+    if (!newNumber) {
+      display.textContent = '';
+      newNumber = true;
+    }
+    display.textContent += evn.target.textContent;
   }
-  if (marker === markerO) {
-    playerO.push(Number(evn.target.dataset.id));
+  if (evn.target.textContent === 'CE') {
+    a = '';
+    display.textContent = '0';
   }
-
-  marker === markerX ? (marker = markerO) : (marker = markerX);
-
-  if (checkWin(playerX)) {
-    colorWin(checkWin(playerX));
-    winnersName = 'гравець X';
-    message.textContent = `виграв ${winnersName}`;
-    showModal();
-    return;
+  if (evn.target.textContent === 'DEL' && display.textContent.length) {
+    let arr = display.textContent.split('');
+    arr.pop();
+    display.textContent = arr.join('');
   }
-
-  if (checkWin(playerO)) {
-    colorWin(checkWin(playerO));
-    winnersName = 'гравець O';
-    message.textContent = `виграв ${winnersName}`;
-    showModal();
-    return;
+  if (evn.target.textContent === '+/-' && display.textContent) {
+    a = Number(display.textContent) * -1;
+    display.textContent = a;
+  }
+  if (evn.target.textContent === '%' && display.textContent) {
+    a = Number(display.textContent) / 100;
+    display.textContent = a;
   }
 
-  if ([...playerO, ...playerX].length === 9) {
-    message.textContent = 'Нічия...';
-    showModal();
+  if (evn.target.classList.contains('operations')) {
+    a = Number(display.textContent);
+    newNumber = false;
+    operation = evn.target.textContent;
+  }
+
+  if (evn.target.textContent === '=' && a) {
+    b = Number(display.textContent);
+    switch (operation) {
+      case '+':
+        newNumber = false;
+        a = a + b;
+        display.textContent = a;
+        b = 0;
+        break;
+      case '-':
+        newNumber = false;
+        a = a - b;
+        display.textContent = a;
+        b = 0;
+        break;
+      case 'x':
+        newNumber = false;
+        a = a * b;
+        display.textContent = a;
+        b = 0;
+        break;
+      case '/':
+        a = a / b;
+        newNumber = false;
+        display.textContent = a;
+        b = 0;
+        break;
+    }
   }
 }
