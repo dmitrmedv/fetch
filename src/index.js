8cda73ad51c8b52465d6434c8bd5687807a57d8c
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2024-06-26 17:24:44 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-import UnsplashAPI from './api';
+// import UnsplashAPI from './api';
 
-const unsplashApi = new UnsplashAPI();
+const searchForm = document.querySelector('.js-search-form');
+const searchButton = document.querySelector('.js-search-button');
+const box = document.querySelector('.box');
 
-unsplashApi.getImgs().then(data => console.log(data));
+const services = [
+  'абонслужба',
+  'ЕХЗ',
+  'Коржов',
+  'група з  приєднань',
+  'СЕУГиДВ',
+  'СКБСГ',
+  'СКПГА',
+  'СТОВГ',
+  'СУГ',
+  'Сусуловський',
+  'Фін. відділ',
+  'Юр. відділ',
+  'ВТВ',
+  'Гол.інж',
+  'Манишев',
+  'ГРП',
+  'Боєжов',
+  'Ревіз. відділ',
+  'АДС',
+  'ГВіП',
+];
+
+// const unsplashApi = new UnsplashAPI();
+
+// function makerMurckup(data) {
+//   return data
+//     .map(({ alt_description, urls: { regular } }) => {
+//       return `<img src=${regular} alt=${alt_description}>`;
+//     })
+//     .join('');
+// }
+
+// unsplashApi.getImgs().then(data => (box.innerHTML = makerMurckup(data)));
