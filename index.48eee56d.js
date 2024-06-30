bb291ac193e881f21bf782f2787b4581cd7d4eae
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2024-06-30 06:38:01 +0000

    Deploying to gh-pages from @ dmitrmedv/fetch@e98440ab3348733f3b6e03b8ad63e7f84ee1baa9 🚀

---
+++ index.48eee56d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelectorAll(".data"),e=document.querySelector("tr");console.log(e.lastEllementChild),t.forEach((function(t){t.textContent.length>=90&&(t.textContent=t.textContent.slice(0,100)+"...")}))}();
+//# sourceMappingURL=index.48eee56d.js.map
---
+++ index.de96b557.js
@@ -0,0 +1,2 @@
+const t=document.querySelectorAll(".data"),e=document.querySelector("tr");console.log(e.lastEllementChild),t.forEach((t=>{t.textContent.length>=90&&(t.textContent=t.textContent.slice(0,100)+"...")}));
+//# sourceMappingURL=index.de96b557.js.map
