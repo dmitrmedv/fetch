e98440ab3348733f3b6e03b8ad63e7f84ee1baa9
dmitrmedv <dmitrmedv@gmail.com>
2024-06-30 09:37:42 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,52 +1,21 @@
-import UnsplashAPI from './api';
-import Masonry from 'masonry-layout';
-// import InfiniteScroll from 'infinite-scroll';
+// const { default: axios } = require('axios');
 
-const gallery = document.querySelector('.gallery');
-const card = document.querySelector('.card');
+// axios.get('http://localhost:3000/posts').then(({ data }) => console.log(data));
 
-const form = document.querySelector('.search-form');
+// const config = {
+//   headers: {
+//     'Content-type': 'applocation/json',
+//   },
+// };
 
-form.addEventListener('submit', onSubmit);
+const data = document.querySelectorAll('.data');
 
-const unsplashAPI = new UnsplashAPI();
+const tr = document.querySelector('tr');
 
-let msnry;
+console.log(tr.lastEllementChild);
 
-function makeCard(data) {
-  console.log(data);
-  return data
-    .map(({ urls: { small }, alt_description }) => {
-      return `<div class="photo-card">
-              <img src=${small} alt=${alt_description} loading="lazy" />
-            </div>`;
-    })
-    .join('');
-}
-
-function onSubmit(even) {
-  even.preventDefault();
-  unsplashAPI.query = even.target.elements.searchQuery.value;
-  even.target.reset();
-  unsplashAPI.getImgs().then(({ results }) => {
-    gallery.insertAdjacentHTML('beforeend', makeCard(results));
-    if (!msnry) {
-      msnry = new Masonry(gallery, {
-        itemSelector: '.photo-card',
-        columnWidth: '.photo-card',
-        percentPosition: true,
-      });
-    } else {
-      msnry.reloadItems();
-      msnry.layout();
-    }
-    new Masonry(gallery);
-    msnry.layout();
-  });
-}
-
-// let infScroll = new InfiniteScroll('.gallery', {
-//   path: '.pagination__next',
-//   append: '.post',
-//   history: false,
-// });
+data.forEach(item => {
+  if (item.textContent.length >= 90) {
+    item.textContent = item.textContent.slice(0, 100) + '...';
+  }
+});
