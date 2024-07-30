e40bd96c8d6ae83c76aa7fef7de8feaf0e327c78
dmitrmedv <dmitrmedv@gmail.com>
2024-07-30 14:38:45 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+fetch('http://localhost:3000/posts')
+  .then(resp => resp.json())
+  .then(data => console.log(data));
 
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+  },
+  body: JSON.stringify({
+    name: 'Іванов В.І.',
+    address: 'Дерибасівська, 34',
+    question: 'перерахунок',
+  }),
+};
+
+fetch('http://localhost:3000/posts', options)
+  .then(resp => resp.json())
+  .then(data => console.log(data));
+fetch('http://localhost:3000/posts', options)
+  .then(resp => resp.json())
+  .then(data => console.log(data));
