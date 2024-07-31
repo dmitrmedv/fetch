b36ab13915227407ddf8c7f8f8bc030d723f16fe
dmitrmedv <dmitrmedv@gmail.com>
2024-07-31 09:34:14 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 const form = document.querySelector('.form');
 const list = document.querySelector('.list');
 
+let count;
+
 function getList() {
   fetch('http://localhost:3000/letters')
     .then(resp => resp.json())
-    .then(data => (list.innerHTML = render(data)));
+    .then(data => {
+      if (data.length !== 0) {
+        count = data[data.length - 1].number + 1;
+      } else {
+        count = 1;
+      }
+
+      let dataSotr = data.sort((a, b) => b.number - a.number);
+      list.innerHTML = render(dataSotr);
+    });
 }
 
 getList();
 
 form.addEventListener('submit', onSubmit);
+
+list.addEventListener('click', deleteLetter);
+
+function deleteLetter(e) {
+  if (e.target.classList.contains('del-btn')) {
+    console.log(e.target.dataset.id);
+    fetch(`http://localhost:3000/letters/${e.target.dataset.id}`, {
+      method: 'DELETE',
+    }).then(() => getList());
+  }
+}
 
 function onSubmit(e) {
   e.preventDefault();
@@ -17,6 +39,7 @@ function onSubmit(e) {
     name: e.target.elements.name.value,
     lastname: e.target.elements.lastname.value,
     addres: e.target.elements.addres.value,
+    number: count,
   };
   fetch('http://localhost:3000/letters', {
     method: 'POST',
@@ -25,6 +48,7 @@ function onSubmit(e) {
   })
     .then(() => {
       getList();
+      count += 1;
     })
     .finally(() => {
       e.target.reset(), e.target.elements.name.focus();
@@ -33,10 +57,12 @@ function onSubmit(e) {
 
 function render(arr) {
   return arr
-    .map(({ addres, name, lastname }) => {
+    .map(({ addres, name, lastname, id, number }) => {
       return `<li>
+   <p>вхідний номер: ${number}к</p>   
 <p>замовник: ${name} ${lastname}</p>
 <p>адреса: ${addres}</p>
+<button type='button' class='del-btn' data-id=${id}>delete</button>
 </li>`;
     })
     .join('');
